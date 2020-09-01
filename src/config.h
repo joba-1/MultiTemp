18e6f39e7312fa079c369015ec37ecda076ca3e0
joba-1 <Joachim.Banzhaf@gmail.com>
2020-09-01 17:27:46 +0200

    Manual calibration (+/- 0.1°C)

--- src/config.h
+++ src/config.h
@@ -24,15 +24,20 @@
 
 #define ONLINE_LED_PIN   D4
 
+// Calibration
+#define B_DIFF 16
+#define V_DIFF 114
+#define R_DIFF -100
+
 // Analog samples for averaging
 #define A_SAMPLES        10
-// default gain 2/3: Vmax=6.144V => 32767*3.3/6.144 
-#define A_MAX            17600
+// default gain 2/3: Vmax=6.144V, Vcc = 3.3-0.111 => 32767*3.189/6.144 
+#define A_MAX            (32767*(3300-V_DIFF)/6144)
 
 // NTC parameters and voltage divider resistor
-#define NTC_B            3950
+#define NTC_B            (3950+B_DIFF)
 #define NTC_R_N          100000
 #define NTC_T_N          25
-#define NTC_R_V          32900              
+#define NTC_R_V          (33000+R_DIFF)      
 
 #endif
